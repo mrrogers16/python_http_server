2d52224bbe224f32babd9e953173d4fec27e49d4
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2024-02-25 14:21:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -73,51 +73,56 @@ def send_404(connectionSocket):
         print(f"Error sending 404: {e}")
 
 
+def main():
+
+    global shutdown_server
+
+    # Try to cast command line port argument to integer
+    try:
+        serverPort = int(sys.argv[1])
+    except ValueError:
+        print("Port number must be an integer.")
+        sys.exit(1)
+
+    try:
+        # Create a TCP server socket (AF_INET for IPv4 protocols, SOCK_STREAM for TCP)
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+
+        # Bind socket to server address and port, '' indicates listening on all interfaces
+        serverSocket.bind(("", serverPort))
+
+        # Listen for incoming connections
+        serverSocket.listen(1)
+
+    # Handle Socket errors
+    except OSError as e:
+        print(f"Socket error: {e.strerror}")
+        sys.exit(1)
+    # Handle other exceptions
+    except Exception as e:
+        print(f"An error occured: {e}")
+        sys.exit(1)
+
+    try:
+        while not shutdown_server:
+            print("Ready to serve...")
+            connectionSocket, addr = serverSocket.accept()
+            if shutdown_server:
+                connectionSocket.close()
+                break
+            clientThread = threading.Thread(
+                target=handle_client, args=(connectionSocket, addr)
+            )
+            clientThread.start()
+    except KeyboardInterrupt:
+        # Ctrl+C to terminate server
+        print(" Server terminated by user.")
+        serverSocket.close()
+
+
 # Check for command line args and port number
 if len(sys.argv) != 2:
     print("Too few arguments")
     print(f"Usage: python3 {sys.argv[0]} <PortNumber>")
-    sys.exit(1)
-
-# Try to cast command line port argument to integer
-try:
-    serverPort = int(sys.argv[1])
-except ValueError:
-    print("Port number must be an integer.")
-    sys.exit(1)
-
-try:
-    # Create a TCP server socket (AF_INET for IPv4 protocols, SOCK_STREAM for TCP)
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-
-    # Bind socket to server address and port, '' indicates listening on all interfaces
-    serverSocket.bind(("", serverPort))
-
-    # Listen for incoming connections
-    serverSocket.listen(1)
-
-# Handle Socket errors
-except OSError as e:
-    print(f"Socket error: {e.strerror}")
-    sys.exit(1)
-# Handle other exceptions
-except Exception as e:
-    print(f"An error occured: {e}")
-    sys.exit(1)
-
-try:
-    while not shutdown_server:
-        print("Ready to serve...")
-        connectionSocket, addr = serverSocket.accept()
-        if shutdown_server:
-            connectionSocket.close()
-            break
-        clientThread = threading.Thread(
-            target=handle_client, args=(connectionSocket, addr)
-        )
-        clientThread.start()
-except KeyboardInterrupt:
-    # Ctrl+C to terminate server
-    print(" Server terminated by user.")
-    serverSocket.close()
-    sys.exit()  # Terminate the program after sending data
+    sys.exit(1)  # Terminate the program after sending data
+main()
